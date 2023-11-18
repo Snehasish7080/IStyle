854a5706986dc279fa286f6acf0442750b9ebee2
Snehasish7080 <snehasish@byzwiz.com>
2023-11-18 10:01:48 +0530

    feat(tag screen): update tag scrren

--- src/screens/CategoryScreen/CategoryScreen.tsx
+++ src/screens/CategoryScreen/CategoryScreen.tsx
@@ -13,6 +13,7 @@ import BackHeader from '../../molecules/BackHeader/BackHeader';
 import SearchIcon from '../../atoms/SearchIcon/SearchIcon';
 import AppText from '../../atoms/AppText/AppText';
 import {Colors} from '../../utils/theme';
+import CloseIcon from '../../atoms/CloseIcon/CloseIcon';
 
 const tagList = [
   'Casual',
@@ -49,6 +50,20 @@ const CategoryScreen: React.FC<ParentNavProps<'CategoryScreen'>> = ({
   route,
 }) => {
   const [search, setSearch] = useState<string>('');
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+
+  const onClickTag = (value: string) => {
+    const temp = [...selectedTags];
+    const index = temp.findIndex(x => x === value);
+
+    if (index < 0) {
+      temp.push(value);
+    } else {
+      temp.splice(index, 1);
+    }
+
+    setSelectedTags(temp);
+  };
   return (
     <Container style={styles.container}>
       <BackHeader
@@ -69,14 +84,69 @@ const CategoryScreen: React.FC<ParentNavProps<'CategoryScreen'>> = ({
           />
         </TouchableOpacity>
 
-        <ScrollView contentContainerStyle={{paddingVertical: 16}}>
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{paddingTop: 30, paddingBottom: 0}}>
+          {selectedTags.map((item, index) => {
+            return (
+              <View key={index} style={styles.selectedTag}>
+                <AppText lineHeight={12} style={styles.tag}>
+                  {item}
+                </AppText>
+                <TouchableOpacity
+                  style={styles.remove}
+                  onPress={() => {
+                    onClickTag(item);
+                  }}
+                  activeOpacity={1}>
+                  <CloseIcon size={18} />
+                </TouchableOpacity>
+              </View>
+            );
+          })}
+        </ScrollView>
+
+        <View style={styles.separatorContainer}>
+          <View style={styles.separator} />
+          <AppText lineHeight={18} style={styles.separatorText}>
+            Tags
+          </AppText>
+          <View style={styles.separator} />
+        </View>
+
+        <ScrollView
+          contentContainerStyle={{paddingVertical: 16}}
+          showsVerticalScrollIndicator={false}>
           <View style={styles.tagListContainer}>
             {tagList.map((item, index) => {
               if (Boolean(search)) {
                 if (item.toLowerCase().includes(search.toLowerCase())) {
                   return (
-                    <TouchableOpacity key={index} style={styles.tagList}>
-                      <AppText lineHeight={12} style={styles.tag}>
+                    <TouchableOpacity
+                      key={index}
+                      style={[
+                        styles.tagList,
+                        {
+                          borderWidth: selectedTags.includes(item) ? 0 : 1,
+                          backgroundColor: selectedTags.includes(item)
+                            ? Colors.dark
+                            : Colors.white,
+                        },
+                      ]}
+                      onPress={() => {
+                        onClickTag(item);
+                      }}>
+                      <AppText
+                        lineHeight={12}
+                        style={[
+                          styles.tag,
+                          {
+                            color: selectedTags.includes(item)
+                              ? Colors.white
+                              : Colors.dark,
+                          },
+                        ]}>
                         {item}
                       </AppText>
                     </TouchableOpacity>
@@ -86,8 +156,31 @@ const CategoryScreen: React.FC<ParentNavProps<'CategoryScreen'>> = ({
                 }
               }
               return (
-                <TouchableOpacity key={index} style={styles.tagList}>
-                  <AppText lineHeight={12} style={styles.tag}>
+                <TouchableOpacity
+                  activeOpacity={1}
+                  key={index}
+                  style={[
+                    styles.tagList,
+                    {
+                      borderWidth: selectedTags.includes(item) ? 0 : 1,
+                      backgroundColor: selectedTags.includes(item)
+                        ? Colors.dark
+                        : Colors.white,
+                    },
+                  ]}
+                  onPress={() => {
+                    onClickTag(item);
+                  }}>
+                  <AppText
+                    lineHeight={12}
+                    style={[
+                      styles.tag,
+                      {
+                        color: selectedTags.includes(item)
+                          ? Colors.white
+                          : Colors.dark,
+                      },
+                    ]}>
                     {item}
                   </AppText>
                 </TouchableOpacity>
